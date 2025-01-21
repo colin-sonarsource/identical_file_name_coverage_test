e8e0b15cff7752e1c35585ebe3fa28cd8c90c9e2
Your Name <you@example.com>
2025-01-21 14:56:10 +0100

    Remove second calculator app tests

--- app2/calculator.test.js
+++
@@ -1,9 +0,0 @@
-const { add, subtract } = require('./calculator');
-
-test('add function', () => {
-  expect(add(1, 2)).toBe(3);
-});
-
-test('subtract function', () => {
-  expect(subtract(10, 5)).toBe(5);
-});
